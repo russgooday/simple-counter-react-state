11d82664b679e726fa84aae04fa593ef73c4a225
Russell Gooday <rpg_digital@yahoo.co.uk>
2021-02-02 00:23:00 +0000

    update

--- simple-counter-react-state/src/Counter.js
+++ simple-counter-react-state/src/Counter.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
+  state = { counter: 0 }
+
+  increment = () => {
+    this.setState(
+      (state, props) => ({ counter: state.counter + 1 })
+    )
+  }
+
+  decrement = () => {
+    this.setState(
+      (state, props) => ({ counter: state.counter - 1 })
+    )
+  }
+
+  reset = () => {
+    this.setState(
+      (state, props) => ({ counter: 0 })
+    )
+  }
 
   render() {
+    const {counter} = this.state
+
     return (
       <div className='Counter'>
-        <p className='count'>0</p>
+        <p className='count'>{counter}</p>
         <section className='controls'>
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
